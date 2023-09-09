df09128c352de01ad816dae3a6fa1e37c3d68ccc
Dharnish38 <dharnishrs38@gmail.com>
2023-09-10 00:17:37 +0530

    Project files

---
+++ MyFirstApi/Controllers/StudentController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet,Route("GetStudents")]
+        public IActionResult GetStudents()
+        {
+            string[] students = { "Rohan", "karan", "jeson" };
+            return StatusCode(200, students);
+        }
+        //public IActionResult GetFirstStudent()
+        //{
+        //    string[] students = { "karan", "rohan", "jeson" };
+        //    return StatusCode(200, students);
+        //}
+
+        [HttpPost,Route("add")]
+        public IActionResult Add(string name)
+        {
+            return Ok("Student Added");
+        }
+    }
+    
+}
